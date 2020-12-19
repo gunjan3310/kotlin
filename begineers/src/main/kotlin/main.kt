c6dd6458e582abf41c754ea9aaf637ef4eb8655a
Gunjan Yadav <gunjanyadav@gunjanyadav.security-station>
2020-12-19 18:20:12 -0500

    kotlin repository initialized

---
+++ begineers/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //this is question 1's solution
+    extremely_basic()
+}
+
+fun extremely_basic(){
+    val a: Int = readLine()!!.toInt()
+    val b: Int = readLine()!!.toInt()
+
+    var x: Int = a + b
+    println(x)
+}
